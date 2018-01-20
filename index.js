fab5ecf46756c2e087f32068e8c26e8f1408616d
Josh Tai <jitai@ucsc.edu>
2018-01-20 02:02:55 -0800

    test again v1

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   response.send('Hello World!')
-})
+})*/
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
