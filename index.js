409214958c2bbc716792cf2082c2c9222a6c4de1
Josh Tai <jitai@ucsc.edu>
2018-01-20 02:22:18 -0800

    asdf

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ var path = require('path');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-/*app.get('/', function(request, response) {
+app.get('/', function(request, response) {
   response.send('Hello World!')
-})*/
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+})
+/*app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
-
+*/
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
