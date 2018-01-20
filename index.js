bc1266bd366f069592411d514455e65867f3ecc6
Josh Tai <jitai@ucsc.edu>
2018-01-20 11:36:37 -0800

    test opentok

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require('express')
 var app = express()
 var path = require('path');
+//var OpenTok = require('opentok'),
+//    opentok = new OpenTok(apiKey, apiSecret);
+
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/test.html'))
 
-app.get('/page2', function(request, response) {
+/*app.get('/page2', function(request, response) {
   //response.send("Hello You!");
-  //response.sendFile(__dirname + '/test.html');
+  response.sendFile(__dirname + '/public/index.html');
 })
 /*app.get('/', function (req, res) {
 */
