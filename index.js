11c6461705d740159e155d97abc4a04ce9714c04
Josh Tai <jitai@ucsc.edu>
2018-01-20 14:51:57 -0800

    heroku pls

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var path = require('path');
 //    opentok = new OpenTok(apiKey, apiSecret);
 
 app.set('port', (5000))
-/*app.get('/',function(req,res){
+app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/public/Landing.html'));
   //__dirname : It will resolve to your project folder.
-});*/
+});
 app.use(express.static(__dirname + '/public'))
 //app.use(express.static(path.join(__dirname, '/public')))
 
