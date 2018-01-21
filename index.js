76e7bf532eb1d994ebb4049591ec18d7b82f3fe9
Josh Tai <jitai@ucsc.edu>
2018-01-20 18:17:26 -0800

    I think it's fixed

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ var path = require('path');
 //var OpenTok = require('opentok'),
 //    opentok = new OpenTok(apiKey, apiSecret);
 
-<<<<<<< HEAD
+
 app.set('port', (process.env.PORT || 5000))
 console.log('working');
-=======
-app.set('port', (5000))
->>>>>>> e1ee107099e86e38388e7de8601cda1093aed53a
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/public/Landing.html'));
   //__dirname : It will resolve to your project folder.
