a1c17a2869bee39eb6c380a5cbe46fea43cbfde0
Josh Tai <jitai@ucsc.edu>
2018-01-20 20:02:18 -0800

    tokbox starting up

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use(express.static(__dirname + '/public'))
 app.get('/landing', function (req, res) {
   res.sendFile(__dirname + '/public/Landing.html');
 })
+app.get('/Tutoring', function (req, res) {
+  //res.sendFile(__dirname + '/public/Landing.html');
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
